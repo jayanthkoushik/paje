e81846e7c8bd038ab35db7fcc0c19196fecf2731
Jayanth Koushik <jnkoushik@gmail.com>
2021-05-30 17:07:58 -0400

    Minify css/js files together

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 ---
 permalink: assets/main
 ---
-{%- capture customjs -%}
+{%- capture alljs -%}
+  {%- include_relative _jquery-slim.min.js -%}
+  {%- include_relative _bootstrap.min.js -%}
+  {%- include_relative _katex.min.js -%}
+  {%- include_relative _auto-render.min.js -%}
   {%- include_relative _custom.js -%}
 {%- endcapture -%}
-{%- include_relative _jquery-slim.min.js -%}
-{%- include_relative _bootstrap.min.js -%}
-{%- include_relative _katex.min.js -%}
-{%- include_relative _auto-render.min.js -%}
-{{- customjs | uglify -}}
+{{- alljs | uglify -}}
