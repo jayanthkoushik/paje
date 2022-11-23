0ebf5c10747126c3962a256cc256ec1dbf22f8a3
Jayanth Koushik <mail@jkoushik.me>
2022-11-22 22:38:27 -0500

    Fix git handling

--- main.sh
+++ main.sh
@@ -4,24 +4,22 @@ set -e
 
 eval "${INPUT_SETUPSCRIPT}"
 
-cp -r /www ${GITHUB_SHA}
-cd ${GITHUB_SHA}
+cd /www
 if [ "${INPUT_NO404}" = true ]; then
     rm 404.md
 fi
 bundle exec jekyll build
-cd ..
+
+mkdir "/target-${GITHUB_SHA}"
+cd "/target-${GITHUB_SHA}"
+git init
 
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git config remote.origin.url "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
+git pull --depth=1 origin "${INPUT_TARGETBRANCH}" || git checkout -b "${INPUT_TARGETBRANCH}"
 
-git fetch --depth=1 origin "${INPUT_TARGETBRANCH}" || true
-git checkout -t origin/"${INPUT_TARGETBRANCH}" || git checkout -b "${INPUT_TARGETBRANCH}"
-
-rsync -r --delete --exclude=${GITHUB_SHA} --exclude=.git/ "${GITHUB_SHA}/_site/" ./
-rm -rf ${GITHUB_SHA}
-
+rsync -r --delete --exclude=.git/ /www/_site/ ./
 git add .
 set +e
 git commit -m "ci: deploy with paje"
