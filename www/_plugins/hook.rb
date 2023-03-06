b9c0b48603b5e86b3952c9209e90063fcf534f84
Jayanth Koushik <mail@jkoushik.me>
2023-03-06 04:08:20 -0500

    Refactor hook code

--- www/_plugins/hook.rb
+++ www/_plugins/hook.rb
@@ -1,15 +1,17 @@
 Jekyll::Hooks.register :pages, :post_init do |page|
-  if page.data["abstract"] then
-    page.content = "# Abstract {#abstract .unnumbered}\n\n#{page.data['abstract']}\n\n" + page.content
+  hdr = ""
+
+  page.data["includes"]&.each do |inc|
+    hdr += "{% include #{inc} %}\n\n"
   end
 
-  if page.data["includes"] then
-    for inc in page.data["includes"].reverse
-      page.content = "{% include #{inc} %}\n\n" + page.content
-    end
+  if page.data["abstract"]
+    hdr += "# Abstract {#abstract .unnumbered}\n\n#{page.data['abstract']}\n\n"
   end
 
-  if page.data["appendices"] then
+  page.content = hdr + page.content
+
+  if page.data["appendices"]
     page.content = page.content + "\n\n<div id='appendices'>\n\n"
     for app in page.data["appendices"]
       page.content = page.content + "---\n\n<div class='appendix'>\n\n{% include #{app} %}\n\n</div>\n\n"
