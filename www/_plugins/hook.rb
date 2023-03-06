1b1c6a312014ffa1aaf41b2030496e831355cce6
Jayanth Koushik <mail@jkoushik.me>
2023-03-05 23:54:43 -0500

    Add hrs before appendices

--- www/_plugins/hook.rb
+++ www/_plugins/hook.rb
@@ -12,7 +12,7 @@ Jekyll::Hooks.register :pages, :post_init do |page|
   if page.data["appendices"] then
     page.content = page.content + "\n\n<div id='appendices'>\n\n"
     for app in page.data["appendices"]
-      page.content = page.content + "<div class='appendix'>\n\n{% include #{app} %}\n\n</div>\n\n"
+      page.content = page.content + "---\n\n<div class='appendix'>\n\n{% include #{app} %}\n\n</div>\n\n"
     end
     page.content = page.content + "</div>\n"
   end
