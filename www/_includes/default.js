7b4880f31eaa60871df63e0fefea80ee6dd39e8a
Jayanth Koushik <mail@jkoushik.me>
2023-03-12 17:47:47 -0400

    Rework theme selection
    * Convert theme toggle to dropdown menu
    
    * Add auto theme option

--- www/_includes/default.js
+++ www/_includes/default.js
@@ -2,32 +2,51 @@ function getPreferedTheme() {
     var storedTheme = localStorage.getItem('theme');
     if (storedTheme === 'dark' || storedTheme === 'light') {
         return storedTheme;
-    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        return 'dark';
-    } else {
-        return 'light';
     }
+    return 'auto';
 }
 
 function setTheme(theme) {
-    $('html').attr('data-bs-theme', theme);
-
-    // svg sources from https://github.com/tabler/tabler-icons
-    if (theme === 'light') {
-        // moon icon
-        $('#theme-switch').html('<svg style="fill: currentColor"><path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" /></svg>');
+    var coreTheme;
+    if (theme === 'dark' || theme === 'light') {
+        coreTheme = theme;
+    } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        coreTheme = 'dark';
     } else {
-        // sun icon
-        $('#theme-switch').html('<svg style="fill: none"><circle cx="12" cy="12" r="4" /><path d="M3 12h1M12 3v1M20 12h1M12 20v1M5.6 5.6l.7 .7M18.4 5.6l-.7 .7M17.7 17.7l.7 .7M6.3 17.7l-.7 .7" /></svg>');
+        coreTheme = 'light';
     }
 
+    // Set global theme.
+    $('html').attr('data-bs-theme', coreTheme);
+
+    // Update themed images.
     $('img[data-darksrc]').attr(
         'src',
         function() {
-            return $(this).attr(theme === 'dark' ? 'data-darksrc' : 'data-lightsrc');
+            return $(this).attr(coreTheme === 'dark' ? 'data-darksrc' : 'data-lightsrc');
         }
     );
+
+    // Update theme selector.
+    $('[data-bs-theme-value]').each(function() {
+        if ($(this).attr('data-bs-theme-value') === theme) {
+            $(this).addClass("active");
+            $("#active-theme-button").html($(this).children("svg").clone());
+        } else {
+            $(this).removeClass("active");
+        }
+    });
 }
+
+$(".theme-button").on("click", function () {
+    var theme = $(this).attr('data-bs-theme-value');
+    if (theme === 'dark' || theme === 'light') {
+        localStorage.setItem('theme', theme);
+    } else {
+        localStorage.removeItem('theme');
+    }
+    setTheme(theme);
+});
 
 window.addEventListener('DOMContentLoaded', () => {
     setTheme(getPreferedTheme());
@@ -35,7 +54,7 @@ window.addEventListener('DOMContentLoaded', () => {
         tabindex: "0",
         role: "button",
         class: "author-link"
-    })
+    });
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
@@ -45,13 +64,9 @@ window.addEventListener('DOMContentLoaded', () => {
     $(".inst-nojs").remove();
 });
 
-document.getElementById('theme-switch').addEventListener('click', () => {
-    var newTheme = $('html').attr('data-bs-theme') === 'dark' ? 'light' : 'dark';
-    setTheme(newTheme);
-    localStorage.setItem('theme', newTheme);
-});
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
-    localStorage.removeItem('theme');
+    if (getPreferedTheme() === 'auto') {
+        setTheme('auto');
+    }
 });
